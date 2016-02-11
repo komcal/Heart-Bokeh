0c656a1f0ad049fe8d5d01f2175dea4bf57b6eb5
komcal <kom.sujautra@gmail.com>
2016-02-12 01:54:20 +0700

    share button

--- public/js/script.js
+++ public/js/script.js
@@ -17,3 +17,14 @@ $("#imgInp").change(function(){
 function Browse(){
   document.getElementById('imgInp').click();
 }
+function Share(){
+  $('#share').click();
+  console.log('cc');
+}
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=1065330750192936";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));

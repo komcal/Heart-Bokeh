67dd40f6fa97458d8a3fac6e3c5dbb65b0fbacf2
komcal <kom.sujautra@gmail.com>
2016-02-13 01:51:02 +0700

    footer edit router

--- app/routes/upload.routes.js
+++ app/routes/upload.routes.js
@@ -1,19 +1,21 @@
 module.exports = function(app){
   var multer = require('multer');
   var upload = require('../controllers/upload.controller');
+  var mime = require('mime');
   app.get('/upload',upload.render);
   var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '.jpg')
-  }
-});
+    destination: function (req, file, cb) {
+      cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '.' + mime.extension(file.mimetype));
+    }
+  });
+
   app.post('/uploaded',multer({
       storage: storage,
       limits: {
-       fileSize: 1000*1000*10, // not sure 10 MB 
+       fileSize: 1000*1000*10, // not sure 10 MB
        files: 1,
        fields: 1
       }

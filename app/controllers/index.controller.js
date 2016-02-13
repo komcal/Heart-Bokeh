6ed502bad6f8b8cbebe9ae902e0ac6eab39f366f
komcal <kom.sujautra@gmail.com>
2016-02-14 01:41:52 +0700

    edit word

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -1,6 +1,5 @@
 exports.render = function(req, res) {
   res.render('index', {
-    'message': 'เปลี่ยนภาพของคุณให้เป็นรูปภาพโบเก้ เพื่อมอบให้คนพิเศษในวันวาเลนไทน์',
     'randomImg': [
       '/pic/1.jpg',
       '/pic/2.jpg',

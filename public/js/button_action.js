125edd640f17cd189b34a18650645c2a22bb4764
komcal <kom.sujautra@gmail.com>
2016-02-13 13:08:00 +0700

    add facebook share

--- public/js/button_action.js
+++ public/js/button_action.js
@@ -8,7 +8,22 @@ function downloadImage() {
 }
 
 function shareOnFacebook() {
-	
+    var pic = document.getElementById('imgShow').src;
+    FB.ui({
+            method: 'feed',
+            link: 'http://research27.ml:3000',
+            name: "Heart Bokeh",
+            description: "มาสร้างูป โบเก้ หัวใจสวยๆ ทำให้รูปของคุณเต็มไปด้วยความรัก กันเลยที่นี่",
+            picture: pic,
+            //picture: "http://research27.ml:3000/game1/game-meeting-result-1.png",
+            display: 'popup'
+    }, function (response) {
+            if (response) {
+                    alert('Success');
+            } else {
+                    alert("Fail");
+            }
+    });
 }
 
 function shareOnTwitter() {

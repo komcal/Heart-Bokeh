34e4f66629c110d56190bb0d9e0009fe09c94c58
SzNeUrTo <Supanut.Overload@gmail.com>
2016-02-14 00:09:52 -0500

    make bokeh select size python

--- app/controllers/makebokeh.controller.js
+++ app/controllers/makebokeh.controller.js
@@ -1,5 +1,5 @@
 exports.exec = function(filename, callback) {
-	var command = "python make-heart-bokeh.py "+ filename + " uploads downloads";
+	var command = "python make-heart-bokeh.py "+ filename + " uploads downloads md";
 	var exec = require('child_process').exec;
 	var process = exec(command);
 
